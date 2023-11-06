6b7caeccc8b3cbee1424c9e34d49f6c7668d15dd
Yann Arif <yannarif@bu.edu>
2023-11-06 09:34:11 -0500

    finished problem6 ps3. add directory and finish p7 lol

---
+++ problem6/TestDriver.java
@@ -0,0 +1,17 @@
+/*
+ * TestDriver
+ * 
+ * A program that can be used for testing your code for Problem 6.
+ */
+
+import java.sql.*;      // needed for the JDBC-related classes 
+import java.io.*;       // needed for file-related classes
+
+public class TestDriver {
+    public static void main(String[] args) 
+        throws ClassNotFoundException, SQLException, FileNotFoundException
+    {
+        // Add your test code below.
+        
+    }
+}
